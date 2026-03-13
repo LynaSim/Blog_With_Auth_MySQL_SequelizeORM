65fdb88d069d4a3810e2d155f1394693ca0dea7d
LynaSim <lyna.cloud@gmail.com>
2026-03-13 20:25:42 +0000

    Added the authentication script, package.json to install dependencies, and a .env.example.

---
+++ utils/auth.js
@@ -0,0 +1,41 @@
+const jwt = require('jsonwebtoken');
+
+const secret = 'mysecretssshhhhhhh';
+const expiration = '2h';
+
+const authMiddleware = (req, res, next) => {
+
+  let token = req.body.token || req.query.token || req.headers.authorization;
+  console.log('token: ' + token);
+
+  if (req.headers.authorization) {
+    token = token.split(' ').pop().trim();
+  }
+
+  if (!token) {
+    res.status(400).json({ message: 'Bearer Token not supplied or invalid' });
+    return;
+  }
+
+  try {
+    const { data } = jwt.verify(token, secret, { maxAge: expiration });
+    req.user = data;
+    next();
+  } catch (err) {
+    console.log('Invalid token');
+    res.status(400).json({ message: 'Invalid token: ' + err.message });
+  }
+
+  return req;
+}
+
+const signToken = (user) => {
+
+  const payload = {
+    id: user.id,
+    email: user.email,
+  };
+  return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+}
+
+module.exports = { authMiddleware, signToken };

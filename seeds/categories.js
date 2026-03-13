b06a2dccf3bbf50ac56600555a5b3e65002aae60
LynaSim <lyna.cloud@gmail.com>
2026-03-13 22:19:48 +0000

    Fixed typo and syntax mistakes in JSON seed data file.

--- seeds/categories.js
+++
@@ -1,8 +0,0 @@
-[
-  { category_name: 'Romance' },
-  { category_name: 'Detective' },
-  { category_name: 'Gothic' },
-  { category_name: 'Humour' },
-  { category_name: 'Science Fiction' },
-  { category_name: 'Fantasy' },
-];
